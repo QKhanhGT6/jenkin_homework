5d261777a7af2708c41629e3ed9b7e3572795824
QKhanhGT6 <153789536+QKhanhGT6@users.noreply.github.com>
2024-12-27 17:40:33 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/version")
+def get_version():
+    return {"version": "1.0.0"}
+
+@app.get("/is_prime/{number}")
+def check_prime(number: int):
+    if number < 2:
+        return {"number": number, "is_prime": False}
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return {"number": number, "is_prime": False}
+    return {"number": number, "is_prime": True}
+
